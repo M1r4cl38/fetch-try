e11a6c039b67b84dd3c9b3c554cd9034e70d28dd
Monika <412.kri@gmail.com>
2023-06-05 22:58:33 +0300

    data url

--- js/main.js
+++ js/main.js
@@ -1,2 +1,11 @@
 const pDOM = document.querySelector('p');
 
+const data = fetch('https://m1r4cl38.github.io/fetch-try//data/services.json')
+    .then((response) => {
+        return response.json();
+    })
+    .then((data) => {
+        for (const icon of data) {
+            console.log(icon);
+        }
+    });
